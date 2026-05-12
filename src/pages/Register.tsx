8b0f7de3ab36f1d13523fc0b486ca28131acf67e
xyp9r <vanaradimov@gmail.com>
2026-05-12 18:32:37 +0200

    style: changed style to white theme, and added sweetalert for an error (reg. error, network error)

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,5 +1,11 @@
 import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
+// Красивые алерты импорт
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+
+// функция сама sweetalert
+const MySwal = withReactContent(Swal);
 
 export default function Register() {
 	const [name, setName] = useState('');
@@ -31,31 +37,41 @@ export default function Register() {
 					navigate('/onboarding');
 
 				} else {
-					alert("❌ Ошибка: " + result.error);
+					MySwal.fire({
+				        icon: "error",
+				        title: "Registration error",
+				        text: result.error || "Error with registration!",
+				        confirmButtonColor: '#3b82f6' // покрасим кнопку в синий цвет Tailwind
+				    });
 				}
 
 		}	catch (error) {
 			console.error("Ошибка при регистрации:", error);
-			alert("Не удалось подключиться к серверу.");
+			MySwal.fire({
+				        icon: "error",
+				        title: "Network error",
+				        text: "IUnable to connect to the server. Please check your internet connection or try again later.",
+				        confirmButtonColor: '#3b82f6' // покрасим кнопку в синий цвет Tailwind
+				    });
 		}
 	};
 
 	return (
-			<div className="min-h-screen bg-slate-950 flex items-center justify-center p-4 font-sans">
-				<div className="w-full max-w-md bg-slate-900 rounded-2xl shadow-2xl p-8 border border-slate-800">
+			<div className="min-h-screen bg-blue-50 flex items-center justify-center p-4 font-sans">
+				<div className="w-full max-w-md bg-white rounded-2xl shadow-xl p-8 border border-slate-100">
 					
-					<h2 className="text-3xl font-bold text-blue-400 mb-6 text-center">Регистрация 💧</h2>
+					<h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">Register 💧</h2>
 
 					<form onSubmit={handleRegister} className="space-y-4">
 						{/* ПОЛЕ ИМЯ */}
 						<div>
-							<label className="block text-slate-400 text-sm mb-2">Ваше имя</label>
+							<label className="block text-slate-400 text-sm mb-2">Your Name</label>
 							<input 
 								type="text"
 								value={name}
 								onChange={(e) => setName(e.target.value)}
-								className="w-full bg-slate-800 text-white rounded-lg p-3 outline-none focus:ring-2 focus:ring-blue-500 transition-all"
-								placeholder="Neo"
+								className="w-full bg-white border border-slate-100 shadow-sm text-slate-800 rounded-lg p-3 outline-none focus:ring-2 focus:ring-blue-200 transition-all placeholder:text-slate-400"
+								placeholder="John"
 								required
 							/>
 						</div>
@@ -67,8 +83,8 @@ export default function Register() {
 								type="email"
 								value={email}
 								onChange={(e) => setEmail(e.target.value)}
-								className="w-full bg-slate-800 text-white rounded-lg p-3 outline-none focus:ring-2 focus:ring-blue-500 transition-all"
-								placeholder="neo@matrix.com"
+								className="w-full bg-white border border-slate-100 shadow-sm text-slate-800 rounded-lg p-3 outline-none focus:ring-2 focus:ring-blue-200 transition-all placeholder:text-slate-400"
+								placeholder="water@gmail.com"
 								required
 							/>
 						</div>
@@ -80,7 +96,7 @@ export default function Register() {
 								type="password"
 								value={password}
 								onChange={(e) => setPassword(e.target.value)}
-								className="w-full bg-slate-800 text-white rounded-lg p-3 outline-none focus:ring-2 focus:ring-blue-500 transition-all"
+								className="w-full bg-white border border-slate-100 shadow-sm text-slate-800 rounded-lg p-3 outline-none focus:ring-2 focus:ring-blue-200 transition-all placeholder:text-slate-400"
 								placeholder="••••••••"
 								required
 							/>
@@ -90,14 +106,14 @@ export default function Register() {
 							type="submit"
 							className="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-3 rounded-lg mt-6 transition-colors"
 						>
-							Создать аккаунт
+							Create account
 						</button>
 					</form>
 
 					<p className="mt-6 text-center text-slate-400 text-sm">
-						Уже в системе?{' '}
+						Have an account?{' '}
 						<Link to="/login" className="text-blue-400 hover:underline">
-							Войти
+							Log In
 						</Link>
 					</p>
 
