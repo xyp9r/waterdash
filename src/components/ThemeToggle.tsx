7e93d86eaa3168237f614e19e6b9fa5459bd9f9b
xyp9r <vanaradimov@gmail.com>
2026-05-18 21:03:40 +0200

    fix: deleted toggle for changing theme for dashboard, ill adding another button for this rn

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,10 +1,19 @@
 // Импортируем кастомный хук темы
 import { useTheme } from '../context/ThemeContext';
-
+import { useLocation } from 'react-router-dom';
 
 export default function ThemeToggle() {
 	// Достаем хук
 	const {theme, toggleTheme} = useTheme();
+
+	// Локация, чтобы можно было убрать переключатель тем из дашборда
+	const location = useLocation();
+
+	const allowedPaths = ['/onboarding', '/register', '/login'];
+
+	const showComponent = allowedPaths.includes(location.pathname);
+
+	if (showComponent === false) return null;
 
 	return (
 			<button
